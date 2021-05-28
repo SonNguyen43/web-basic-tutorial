d3e8eb0e3cca4d5ef139907ddd18481d9b451fde
Nguyễn Bình Hồng Sơn <59218212+SonNguyen43@users.noreply.github.com>
2021-05-28 23:04:49 +0700

    update

--- 1_HTML-CSS-Zero-To-Hero/9_Luxstay/assets/js/app.js
+++ 1_HTML-CSS-Zero-To-Hero/9_Luxstay/assets/js/app.js
@@ -1,24 +1,38 @@
-function inputActive(parent, child, event) {
+function changeImg(src) {
+  document.querySelector("#slide__img").src  = src;
+}
+
+function inputActive(parent, child, action, event) {
   document.querySelector(child).addEventListener(event, (e) => {
-    document.querySelector(parent).classList.add("input-active");
+    document.querySelector(parent).classList.add(action);
   });
 }
 
-function inputUnActive(parent, child, event) {
+function inputUnActive(parent, child, action, event) {
   document.querySelector(child).addEventListener(event, (e) => {
-    document.querySelector(parent).classList.remove("input-active");
+    document.querySelector(parent).classList.remove(action);
   });
 }
 
 // input 1
-inputActive(".navbar__search-input--1", ".navbar__search-input--1-text", "focus");
-inputUnActive(".navbar__search-input--1", ".navbar__search-input--1-text", "focusout");
+inputActive(".navbar__search-input--1", ".navbar__search-input--1-text", "input-active", "focus");
+inputUnActive(".navbar__search-input--1", ".navbar__search-input--1-text", "input-active", "focusout");
 
 // input 2
 
-inputActive(".navbar__search-input--2", ".navbar__search-input--2-date", "mouseover");
-inputUnActive(".navbar__search-input--2", ".navbar__search-input--2-date", "mouseout");
+inputActive(".navbar__search-input--2", ".navbar__search-input--2-date", "input-active", "mouseover");
+inputUnActive(".navbar__search-input--2", ".navbar__search-input--2-date", "input-active", "mouseout");
 
 // input 3
-inputActive(".navbar__search-input--3", ".navbar__search-input--3-number", "mouseover");
-inputUnActive(".navbar__search-input--3", ".navbar__search-input--3-number", "mouseout");
+inputActive(".navbar__search-input--3", ".column-2", "input-active", "mouseover");
+inputUnActive(".navbar__search-input--3", ".column-2", "input-active", "mouseout");
+
+// slide show
+inputActive(".slide__choose-item--1", ".slide__choose-item--1", "active", "mouseover");
+inputUnActive(".slide__choose-item--1", ".slide__choose-item--1", "active", "mouseout");
+
+inputActive(".slide__choose-item--2", ".slide__choose-item--2", "active", "mouseover");
+inputUnActive(".slide__choose-item--2", ".slide__choose-item--2", "active", "mouseout");
+
+inputActive(".slide__choose-item--3", ".slide__choose-item--3", "active", "mouseover");
+inputUnActive(".slide__choose-item--3", ".slide__choose-item--3", "active", "mouseout");
